98f702d75ccfe1808988de6f1254f3c795baea2c
petrockblog <contact@petrockblock.com>
2019-06-30 09:09:40 +0200

    Wiringpi is fetcehd from Gordons site as debian package

--- install.sh
+++ install.sh
@@ -7,7 +7,12 @@ if [[ "$(id -u)" -ne 0 ]]; then
 fi
 
 # ensure that all needed OS packages are installed
-apt-get install -y git cmake g++ doxygen build-essential wiringpi || (c=$?; echo "Error during installation of APT packages"; (exit $c))
+apt-get install -y git cmake g++ doxygen build-essential || (c=$?; echo "Error during installation of APT packages"; (exit $c))
+
+pushd /temp
+wget https://project-downloads.drogon.net/wiringpi-latest.deb
+dpkg -i wiringpi-latest.deb
+popd
 
 # ensure that we are within the PowerBlock directory
 currentDirectory=${PWD##*/}
