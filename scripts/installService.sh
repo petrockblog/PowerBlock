b981b0e7e17c33a6e7797d2a97f898d00a6ce684
petrockblog <contact@petrockblock.com>
2019-08-25 09:43:11 +0200

    Extended installservice script to also care for overlay installation

--- scripts/installService.sh
+++ scripts/installService.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+apt-get -Y install device-tree-compiler
+dtc -@ -I dts -O dtb -o gpio_pull-overlay.dtb gpio_pull-overlay.dts
+cp gpio_pull-overlay.dtb /boot/overlays
+grep -qxF 'dtoverlay=gpio_pull' /boot/config.txt || echo 'dtoverlay=gpio_pull' >> /boot/config.txt
+
 install -m 0755 powerblock /etc/init.d
 update-rc.d powerblock defaults
 /etc/init.d/powerblock start
