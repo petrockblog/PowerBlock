5f6b267cb8f172045cc6c00df637a71b986bd9be
petrockblog <info@petrockblock.com>
2019-09-04 22:10:45 +0200

    Removed unnecessary device-tree installation

--- scripts/installService.sh
+++ scripts/installService.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-apt-get install -q -y device-tree-compiler
-dtc -@ -I dts -O dtb -o gpio_pull-overlay.dtb gpio_pull-overlay.dts
-cp gpio_pull-overlay.dtb /boot/overlays
-grep -qxF 'dtoverlay=gpio_pull' /boot/config.txt || echo 'dtoverlay=gpio_pull' >> /boot/config.txt
-
 install -m 0755 powerblock /etc/init.d
 update-rc.d powerblock defaults
 /etc/init.d/powerblock start
 
 echo "Installation of PowerBlock service done."
-echo "!!! Please note that YOU NEED TO REBOOT to let the driver be effective. !!!"
-
